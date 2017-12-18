a1f364927fe781581e40720aec0b939ddade1dec
Nia <niadavislad@gmail.com>
2017-12-18 23:26:09 +0000

    finished react day 2 project

--- index.js
+++ index.js
@@ -5,31 +5,36 @@
 //Part Two: Add a delete button to each list element.
 
 function List(props) {
-    //TODO: Create list from props.
-    return (<ul>
-    </ul>);
+  return (<ul>
+      {
+      props.items.map(function(item, index) {
+        return <li key={index}>{item}</li>;
+      })}
+  </ul>);
 };
 
 var App = React.createClass({
   getInitialState: function() {
-    //TODO: Set the initial state.
     return {
-        
+      term: '',
+      items: []
     };
   },
   onChange: function(event) {
-    //TODO: Save current value in input field.
+    this.setState({ term: event.target.value });
   },
   onSubmit: function(event) {
-      //TODO: Add to list.
+    event.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.term]
+    });
   },
   render: function() {
-    //TODO: Add event handlers to input and button.
     return (
       <div>
         <h2>Todo List</h2>
-        <form className="App">
-          <input/>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
           <button>Add</button>
         </form>
         <List items={this.state.items} />
@@ -41,4 +46,70 @@ var App = React.createClass({
 ReactDOM.render(
   <App/>,
   document.getElementById('container')
+);
+
+var DeleteButton = React.createClass({
+  deleteItem: function() {
+    this.props.onDelete(this.props.index);
+  },
+  render: function() {
+    return (
+      <button onClick={this.deleteItem}>Delete</button>
+    );
+  }
+});
+
+function List(props) {
+  return (<ul>
+      {
+      props.items.map(function(item, index) {
+        return (<li id={index} key={index}>
+            {item} 
+            <DeleteButton index={index} onDelete={props.onDelete}/>
+        </li>);
+      })}
+  </ul>);
+};
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      term: '',
+      items: []
+    };
+  },
+  onChange: function(event) {
+    this.setState({ term: event.target.value });
+  },
+  onSubmit: function(event) {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  },
+  onDelete: function(index) {
+    this.state.items.splice(index, 1);
+    this.setState({
+      items: this.state.items
+    })
+  },
+  render: function() {
+    return (
+      <div>
+        <h2>Todo List</h2>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Add</button>
+        </form>
+        <List items={this.state.items} onDelete={this.onDelete}/>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
 );
